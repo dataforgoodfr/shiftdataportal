07bcfed73be17aa08d6edf8400270c418200d99c
Sébastien Bourgeois <sebastienbourgeois60@gmail.com>
2025-06-20 12:15:48 +0200

    Remove test files

--- data-preparation/sdp_data_preparation/utils/import_test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-from .test import tst_list
-
-df = pd.DataFrame(tst_list)
--- data-preparation/src/utils/test_exclusion_pre_commit.py
+++
@@ -1,7 +0,0 @@
-from .utils import get_project_root_path
-
-import pandas as pd
-
-root_path=   get_project_root_path()
-
-df =pd.DataFrame([root_path ])
