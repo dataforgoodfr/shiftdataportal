cb4d044b7e111ab2868ef15f4777297b21f543b9
Sébastien Bourgeois <sebastienbourgeois60@gmail.com>
2025-07-04 16:39:01 +0200

    Fix comment

--- data-preparation/scripts/opec/01_transform.py
+++ data-preparation/scripts/opec/01_transform.py
@@ -9,7 +9,7 @@ df = opec.stage_proven_reserves_gas(OPEC_DATA_PATH)
 dst_filepath = os.path.join(OPEC_DATA_PATH, "stg_proven_reserves_gas.csv")
 df.to_csv(dst_filepath, index=False)
 
-# Yearly proven reserves of oil
+# Proven reserves of oil
 df = opec.stage_proven_reserves_oil(OPEC_DATA_PATH)
 dst_filepath = os.path.join(OPEC_DATA_PATH, "stg_proven_reserves_oil.csv")
 df.to_csv(dst_filepath, index=False)
